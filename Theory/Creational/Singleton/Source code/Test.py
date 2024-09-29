081e3dac321955f4a9269433c512086033d589e2
tasubaki <namdangutc1048@gmail.com>
2024-09-29 18:15:16 +0700

    Update Lazy Initialization exercise

--- Theory/Creational/Singleton/Source code/Test.py
+++ Theory/Creational/Singleton/Source code/Test.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractstaticmethod
+
 class EagerInitialization:
     # Tạo instance từ đầu khi chương trình bắt đầu
     _instance = None
@@ -20,6 +22,35 @@ class EagerInitialization:
         self.name = name
 
 
+class IPerson(ABC):
+    @abstractstaticmethod
+    def print_data():
+        """ Implement in child class"""
+
+class PersonSingleton(IPerson):
+    """Biến này là name mangling(tức là _PersonSingleton__instance). Biến này được coi là private và sẽ 
+    không thể truy cập từ bên ngoài class một cách trực tiếp (ví dụ: object.__instance sẽ gây lỗi)."""
+    __instance = None
+
+    def __init__(self, name: str, age: int) -> None:
+        if PersonSingleton.__instance is not None:
+            raise Exception("Singleton cannot be instantiated more than once!")
+        else:
+            self.name = name
+            self.age = age
+            PersonSingleton.__instance = self
+
+    @staticmethod
+    # Lazy design patterns
+    def get_instance():
+        if PersonSingleton.__instance is None:
+            PersonSingleton("Default Name", 0)
+        return PersonSingleton.__instance
+
+    @staticmethod
+    def print_data():
+        print(f"Name: {PersonSingleton.__instance.name}, Age: {PersonSingleton.__instance.age}")
+
 # Test the Singleton
 if __name__ == "__main__":
     eager1 = EagerInitialization()
@@ -28,3 +59,14 @@ if __name__ == "__main__":
 
     eager2 = EagerInitialization()
     print(eager2.get_name())        # Dù có bao nhiêu lần gọi EagerInitialization, vẫn chỉ có một instance duy nhất được trả về.
+    
+
+    #test2 
+    test2 = PersonSingleton("taki", 12)
+    print(test2)
+    test2.print_data()
+
+    #test = PersonSingleton("a", 12)
+    test = PersonSingleton.get_instance()
+    print(test)
+    test.print_data()
